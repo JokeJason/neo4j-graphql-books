6005e21428c0f2ad63d70ebd8aecfc4c569ea960
Yiqing Zhu <793831308@qq.com>
2023-07-16 11:12:03 +1000

    Finished integration test for main.ts by adding both beforeAll, and extra test case

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,22 +2,81 @@ import assert = require('node:assert');
 
 import { createServer } from './newServer.js';
 
-it('returns hello with the provided name', async () => {
-  const newServer = await createServer();
-  const response = await newServer.executeOperation({
-    query: 'query ExampleQuery { books { title, author }}',
-    variables: {},
+describe('Neo4j GraphQL integration tests', () => {
+  let newServer;
+
+  beforeAll(async () => {
+    newServer = await createServer();
+
+    // clear database about books
+    await newServer.executeOperation({
+      query: `mutation DeleteBooks {
+        deleteBooks {
+          nodesDeleted
+        }
+      }`,
+      variables: {},
+    });
   });
 
-  // Note the use of Node's assert rather than Jest's expect; if using
-  // TypeScript, `assert`` will appropriately narrow the type of `body`
-  // and `expect` will not.
-  assert(response.body.kind === 'single');
+  it('After initialization, there should be no data in the database', async () => {
+    const response = await newServer.executeOperation({
+      query: `query {
+        books {
+          title
+          author
+        }
+      }`,
+      variables: {},
+    });
 
-  expect(response.body.singleResult.errors).toBeUndefined();
-  expect(response.body.singleResult.data?.books).toHaveLength(3);
-  expect(response.body.singleResult.data?.books[0]).toEqual({
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
+    // Note the use of Node's assert rather than Jest's expect; if using
+    // TypeScript, `assert`` will appropriately narrow the type of `body`
+    // and `expect` will not.
+    assert(response.body.kind === 'single');
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data?.books).toHaveLength(0);
+  });
+
+  it('Add books into database', async () => {
+    const response = await newServer.executeOperation({
+      query: `mutation CreateBooks($input: [BookCreateInput!]!) {
+        createBooks(input: $input) {
+          books {
+            author
+            title
+          }
+        }
+      }`,
+      variables: {
+        input: [
+          {
+            title: 'Harry Potter',
+            author: 'JK R',
+          },
+          {
+            title: 'The Great Gatsby',
+            author: 'F. Scott Fitzgerald',
+          },
+          {
+            title: 'The Two Towers',
+            author: 'JRR Tolkien',
+          },
+        ],
+      },
+    });
+
+    // Note the use of Node's assert rather than Jest's expect; if using
+    // TypeScript, `assert`` will appropriately narrow the type of `body`
+    // and `expect` will not.
+    assert(response.body.kind === 'single');
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data?.createBooks?.books).toHaveLength(3);
+    expect(response.body.singleResult.data?.createBooks?.books[0]).toEqual({
+      title: 'Harry Potter',
+      author: 'JK R',
+    });
   });
 });
